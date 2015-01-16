88d2f3b951ec6f9c745aa02f26af6a2e676ca1ea
Chris Fuhrman <chrisfuhrman@gmail.com>
2015-01-16 15:10:25 -0600

    fix

--- high_low_game.php
+++ high_low_game.php
@@ -1,46 +1,67 @@
 <?php
 
-// High Low Game.  Guess a # between 1 and 100!
-do {
-	fwrite(STDOUT, 'Guess a # between 1 and 100.'); 
-	"\n";
-	$users_number = fgets(STDIN);
-
-	if ($users_number < $random_number) {
-		echo "HIGHER \n"; 
-	}
-	elseif ($users_number > $random_number) {
-		echo "LOWER \n";
-	}
-} while ($random_number != $users_number);
-
-echo "You won!";
-
-exit(0);
+$random_question_array = ['Guess again', 'Try again', 'Enter # here', 'Guess a #'];
+$comment_array = ['Are you even trying?', 'Are you kidding me?', 'You are so wrong!'];
 
 
-// High Low Game variation.
+// High Low Game variation where the user can input the high and low variable.
+
+$low = min ($argv);
+$high = max ($argv[1], $argv[2]);
+
+
+if (!is_numeric($low)) {
+	$low = 1;
+}
+if (!is_numeric($high)) {
+	$high = 100;
+}
+if ($argc != 3) {
+$low = 1;
+$high = 100;
+}
+
+$random_number = mt_rand($low, $high);
+
+// $iteration for computers question.
 $iteration = 0;
-$random_number = mt_rand(1, 100);
+
 do {
+
+do {
+	$iArray = mt_rand(0, 3);
+	$i_comment_array = mt_rand(0, 2);
+
 	if ($iteration == 0) {
-		fwrite(STDOUT, "Guess a # between 1 and 100. \n");
+		fwrite(STDOUT, "\nGuess a # between $low and $high. \n");
 	}
 	elseif ($iteration != 0) {
-		fwrite(STDOUT, "Guess again \n");
+		fwrite(STDOUT, " {$random_question_array[$iArray]}\n");
 	}
 	$users_number = fgets(STDIN);
 
 	if ($users_number < $random_number) {
-		echo "HIGHER \n"; 
+		echo "HIGHER \n {$comment_array[$i_comment_array]}";
 	}
 	elseif ($users_number > $random_number) {
-		echo "LOWER \n";
+		echo "LOWER \n {$comment_array[$i_comment_array]}";
 	}
 	$iteration++;
 } while ($random_number != $users_number);
+echo "You won!" . PHP_EOL;
+$iteration = 0;
+fwrite(STDOUT, "Play again? \n");
+$playAgain = fgets(STDIN);
 
-echo "You won!";
+
+// Have user enter new # parameters:
+fwrite(STDOUT, "Enter new parameters \n");
+$low = min ($argv);
+$high = max ($argv[1], $argv[2]);
+$random_number = mt_rand($low, $high);
+
+}
+while ($playAgain == ('Yes' || 'Y'));
 
 exit(0);
 
